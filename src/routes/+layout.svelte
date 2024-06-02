81478e26f6ff4223498bb71c6ea7bc95af5912b7
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-06-02 13:23:30 -0400

    chore: added on mount method in layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import 'bulma/css/bulma.min.css';
+    // import { firebaseAuth } from "$lib/firebase/firebase.auth";
+
+
+    // onMount(() => {
+    //     // Listen to token generation
+    //     firebaseAuth.onIdTokenChanged(async (user)=>{
+    //         if(user){
+    //             //send the token to the server
+    //             const token = await user.getIdToken();
+    //             await fetch("/api/auth",{
+    //                 method:'POST',
+    //                 body:JSON.stringify({"token":token})
+    //             })
+    //         }
+    //     })
+    //     firebaseAuth.onAuthStateChanged(async(user) => {
+           
+    //     });
+    // });
+</script>
+
+
+<slot></slot>
