cb7685fbee87ed87c1750bf9a352f16e63ac80d2
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-06-02 13:23:04 -0400

    chore: Update svelte.config.js to preprocess SCSS and handle unused CSS selectors

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+// eslint-disable-next-line no-unused-vars
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: vitePreprocess({
+		scss: {
+			prependData: "@import './src/app.scss';",
+		}
+	}),
+    // Remove vite-plugin-svelte warnings about unused CSS selectors
+    onwarn: (warning, handler) => {
+        const { code } = warning;
+        if (code === 'css-unused-selector') return;
+        handler(warning);
+    },
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
 };
