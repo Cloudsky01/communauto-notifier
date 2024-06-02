bea3e7fc7db9aca18b8ecbd735371f2a98746b24
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-06-02 13:23:41 -0400

    feat: created main landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
 <script>
-    import Geolocation from './geolocation.svelte';
+    import Geolocation from '../components/geolocation.svelte';
 </script>
 
-<main>
-    <Geolocation />
+<header>
+    <nav class="navbar is-primary" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="/">
+            Communauto Notifier
+            </a>
+            <a class="navbar-item" href="/about">
+            About
+            </a>
+            <a class="navbar-item" href="/contact">
+            Contact
+            </a>
+        </div>
+    </nav>
+</header>
+<main class="has-text-centered">
+    <section class="hero is-large">
+        <div class="hero-body">
+          <p class="title">Communauto Notifier</p>
+          <p class="subtitle">A free open source service</p>
+          <div class="container">
+            <Geolocation />
+          </div>
+        </div>
+      </section>
 </main>
-
